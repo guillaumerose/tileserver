720c75fcc1cdb24aaff8e5b5e6f87d75f5eb6dcf
Guillaume Rose <guillaume.rose@mappy.com>
2016-09-04 22:53:37 +0200

    Test

--- src/main/java/fr/guillaumerose/TileFactory.java
+++ src/main/java/fr/guillaumerose/TileFactory.java
@@ -1,63 +1,91 @@
 package fr.guillaumerose;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LineString;
-
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.*;
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.Way;
+import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
+import org.openstreetmap.osmosis.core.task.v0_6.Sink;
+import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader;
 
-import net.morbz.osmonaut.EntityFilter;
-import net.morbz.osmonaut.IOsmonautReceiver;
-import net.morbz.osmonaut.Osmonaut;
-import net.morbz.osmonaut.osm.Entity;
-import net.morbz.osmonaut.osm.EntityType;
-import net.morbz.osmonaut.osm.Tags;
-import net.morbz.osmonaut.osm.Way;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+
 import no.ecc.vectortile.VectorTileEncoder;
 
 public class TileFactory {
-    private static final GeometryFactory geometryFactory = new GeometryFactory();
+	private static final GeometryFactory geometryFactory = new GeometryFactory();
+	private final List<double[]> ways = read();
 
-    public byte[] create(BoundingBox bbox, boolean buidings) {
-        VectorTileEncoder encoder = new VectorTileEncoder();
-        Osmonaut osmonaut = new Osmonaut("/home/grose/projets/github/vector-tiles/1409.osm.pbf", new EntityFilter(false, true, false));
-        osmonaut.scan(new IOsmonautReceiver() {
-            @Override
-            public boolean needsEntity(EntityType type, Tags tags) {
-                return (buidings && tags.hasKeyValue("building", "yes"))
-                        || tags.hasKey("highway")
-                        || tags.hasKey("waterway")
-                        || tags.hasKeyValue("natural", "water")
-                        || tags.hasKeyValue("landuse", "forest")
-                        || tags.hasKeyValue("landuse", "grass")
-                        || tags.hasKeyValue("leisure", "park");
-            }
+	public byte[] create(BoundingBox bbox, boolean buidings) {
+		VectorTileEncoder encoder = new MyEncoder(4096, 8, true);
+		Map<String, String> attributes = new HashMap<>();
+		for (double[] way : ways) {
+			Coordinate[] array = new Coordinate[way.length / 2];
+			for (int i = 0; i < array.length; i++) {
+				array[i] = new Coordinate(bbox.relativeLon(way[2 * i]), bbox.relativeLat(way[2 * i + 1]));
+			}
+			try {
+				encoder.addFeature("building", attributes,
+						geometryFactory.createPolygon(geometryFactory.getCoordinateSequenceFactory().create(array)));
+			} catch (Exception e) {
+				System.out.println("Cannot add building");
+			}
+		}
+		return encoder.encode();
+	}
 
-            @Override
-            public void foundEntity(Entity entity) {
-                Way way = (Way) entity;
-                List<Coordinate> coordinates = way.getNodes().stream()
-                        .map(n -> new Coordinate(bbox.relativeLon(n.getLatlon().getLon()), bbox.relativeLat(n.getLatlon().getLat())))
-                        .collect(toList());
-                Map<String, String> attributes = new HashMap<>();
-                encoder.addFeature(
-                        entity.getTags().hasKeyValue("landuse", "forest") ? "wood"
-                                : (entity.getTags().hasKeyValue("landuse", "grass") || entity.getTags().hasKeyValue("leisure", "park")) ? "park"
-                                        : entity.getTags().hasKeyValue("building", "yes") ? "building"
-                                                : (entity.getTags().hasKeyValue("natural", "water") || entity.getTags().hasKey("waterway")) ? "water" : "highway",
-                        attributes,
-                        line(coordinates));
-            }
-        });
-        return encoder.encode();
-    }
+	private List<double[]> read() {
+		Map<Long, double[]> nodes = new HashMap<>();
+		List<double[]> ways = new ArrayList<>();
+		String filename = "/home/guillaume/projets/osrm-tests/1409.osm.pbf";
+		PbfReader reader = new PbfReader(new File(filename), 4);
+		reader.setSink(new Sink() {
+			@Override
+			public void release() {
+			}
 
-    private static LineString line(List<Coordinate> coordinates) {
-        Coordinate[] array = coordinates.toArray(new Coordinate[coordinates.size()]);
-        return geometryFactory.createLineString(geometryFactory.getCoordinateSequenceFactory().create(array));
-    }
+			@Override
+			public void complete() {
+			}
+
+			@Override
+			public void initialize(Map<String, Object> arg0) {
+			}
+
+			@Override
+			public void process(EntityContainer container) {
+				switch (container.getEntity().getType()) {
+				case Node:
+					Node node = (Node) container.getEntity();
+					nodes.put(node.getId(), new double[] { node.getLongitude(), node.getLatitude() });
+					break;
+				case Way:
+					Way way = (Way) container.getEntity();
+					if (way.getTags().stream().anyMatch(w -> w.getKey().equals("building"))) {
+						double[] coordinates = new double[2 * way.getWayNodes().size()];
+						int i = 0;
+						for (WayNode wn : way.getWayNodes()) {
+							double[] n = nodes.get(wn.getNodeId());
+							coordinates[i] = n[0];
+							coordinates[i + 1] = n[1];
+							i = i + 2;
+						}
+						ways.add(coordinates);
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		});
+		reader.run();
+		System.out.println("Loading OK");
+		return ways;
+	}
 }

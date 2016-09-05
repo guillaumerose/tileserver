19bea37607737f4292db146b3ded48efc9882dec
Guillaume Rose <guillaume.rose@mappy.com>
2016-09-05 13:31:53 +0200

    Refactor

---
+++ src/main/java/fr/guillaumerose/IndexFactory.java
@@ -0,0 +1,78 @@
+package fr.guillaumerose;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
+import org.openstreetmap.osmosis.core.domain.v0_6.Node;
+import org.openstreetmap.osmosis.core.domain.v0_6.Way;
+import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
+import org.openstreetmap.osmosis.core.task.v0_6.Sink;
+import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader;
+
+public class IndexFactory {
+    private final String filename = "/home/grose/projets/github/vector-tiles/1409.osm.pbf";
+
+    public Index create() {
+        long start = System.currentTimeMillis();
+        Map<Long, float[]> nodes = new HashMap<>(500000);
+        Index index = new Index();
+        PbfReader reader = new PbfReader(new File(filename), 4);
+        reader.setSink(new Sink() {
+            @Override
+            public void release() {}
+
+            @Override
+            public void complete() {}
+
+            @Override
+            public void initialize(Map<String, Object> arg0) {}
+
+            @Override
+            public void process(EntityContainer container) {
+                switch (container.getEntity().getType()) {
+                    case Node:
+                        Node node = (Node) container.getEntity();
+                        nodes.put(node.getId(), new float[] { (float) node.getLongitude(), (float) node.getLatitude() });
+                        break;
+                    case Way:
+                        Way way = (Way) container.getEntity();
+                        if (way.getTags().stream().anyMatch(w -> w.getKey().equals("building")) && isPolygon(way)) {
+                            index.index("building", coordinates(nodes, way));
+                        }
+                        else if (way.getTags().stream().anyMatch(w -> w.getKey().equals("waterway")) && isPolygon(way)) {
+                            index.index("water", coordinates(nodes, way));
+                        }
+                        else if (way.getTags().stream().anyMatch(w -> w.getKey().equals("highway"))) {
+                            index.index("highway", coordinates(nodes, way));
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        reader.run();
+        System.out.println(System.currentTimeMillis() - start + "ms");
+        return index;
+    }
+
+    private static float[] coordinates(Map<Long, float[]> nodes, Way way) {
+        float[] coordinates = new float[2 * way.getWayNodes().size()];
+        int i = 0;
+        for (WayNode wn : way.getWayNodes()) {
+            float[] n = nodes.get(wn.getNodeId());
+            coordinates[i] = n[0];
+            coordinates[i + 1] = n[1];
+            i = i + 2;
+        }
+        return coordinates;
+    }
+
+    private static boolean isPolygon(Way way) {
+        List<WayNode> wn = way.getWayNodes();
+        return wn.get(0).getNodeId() == wn.get(wn.size() - 1).getNodeId();
+    }
+}
--- src/main/java/fr/guillaumerose/TileFactory.java
+++ src/main/java/fr/guillaumerose/TileFactory.java
@@ -1,119 +1,45 @@
 package fr.guillaumerose;
 
-import java.io.File;
-import java.util.ArrayList;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequence;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
-import org.openstreetmap.osmosis.core.domain.v0_6.Node;
-import org.openstreetmap.osmosis.core.domain.v0_6.Way;
-import org.openstreetmap.osmosis.core.domain.v0_6.WayNode;
-import org.openstreetmap.osmosis.core.task.v0_6.Sink;
-import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader;
-
-import com.github.davidmoten.rtree.Entry;
-import com.github.davidmoten.rtree.RTree;
-import com.github.davidmoten.rtree.geometry.Geometries;
-import com.github.davidmoten.rtree.geometry.Geometry;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-
-import no.ecc.vectortile.VectorTileEncoder;
+import fr.guillaumerose.Index.StoredPolygon;
+import fr.guillaumerose.encoder.MyEncoder;
 
 public class TileFactory {
-	private static final GeometryFactory geometryFactory = new GeometryFactory();
-	private final RTree<double[], Geometry> tree = read();
+    private static final GeometryFactory geometryFactory = new GeometryFactory();
+    private final Index tree = new IndexFactory().create();
 
-	public byte[] create(BoundingBox bbox, boolean buidings) {
-		VectorTileEncoder encoder = new MyEncoder(4096, 8, true);
-		Map<String, String> attributes = new HashMap<>();
-		Iterable<Entry<double[], Geometry>> found = tree.search(bbox.rectangle()).toBlocking().toIterable();
-		AtomicInteger counter = new AtomicInteger(0);
-		found.forEach(en -> {
-			double[] way = en.value();
-			Coordinate[] array = new Coordinate[way.length / 2];
-			for (int i = 0; i < array.length; i++) {
-				array[i] = new Coordinate(bbox.relativeLon(way[2 * i]), bbox.relativeLat(way[2 * i + 1]));
-			}
-			try {
-				encoder.addFeature("water", attributes,
-						geometryFactory.createPolygon(geometryFactory.getCoordinateSequenceFactory().create(array)));
-			} catch (Exception e) {
-				System.out.println("Cannot add building");
-			}
-			counter.incrementAndGet();
-		});
-		System.out.println(counter.get() + "/" + tree.size());
-		return encoder.encode();
-	}
+    public byte[] create(BoundingBox bbox, boolean buidings) {
+        MyEncoder encoder = new MyEncoder();
+        Map<String, String> attributes = new HashMap<>();
+        AtomicInteger counter = new AtomicInteger(0);
+        tree.search(bbox).forEach(stored -> {
+            if (buidings || !stored.getLayer().equals("building")) {
+                encoder.addFeature(stored.getLayer(), attributes, linestring(bbox, stored));
+                counter.incrementAndGet();
+            }
+        });
+        System.out.println(counter.get() + "/" + tree.size());
+        return encoder.encode();
+    }
 
-	private RTree<double[], Geometry> read() {
-		Map<Long, double[]> nodes = new HashMap<>();
-		List<double[]> ways = new ArrayList<>();
-		String filename = "/home/guillaume/projets/osrm-tests/1409.osm.pbf";
-		PbfReader reader = new PbfReader(new File(filename), 4);
-		reader.setSink(new Sink() {
-			@Override
-			public void release() {
-			}
+    public LineString linestring(BoundingBox bbox, StoredPolygon stored) {
+        return geometryFactory.createLineString(sequence(bbox, stored));
+    }
 
-			@Override
-			public void complete() {
-			}
-
-			@Override
-			public void initialize(Map<String, Object> arg0) {
-			}
-
-			@Override
-			public void process(EntityContainer container) {
-				switch (container.getEntity().getType()) {
-				case Node:
-					Node node = (Node) container.getEntity();
-					nodes.put(node.getId(), new double[] { node.getLongitude(), node.getLatitude() });
-					break;
-				case Way:
-					Way way = (Way) container.getEntity();
-					if (way.getTags().stream().anyMatch(w -> w.getKey().equals("waterway"))) {
-						double[] coordinates = new double[2 * way.getWayNodes().size()];
-						int i = 0;
-						for (WayNode wn : way.getWayNodes()) {
-							double[] n = nodes.get(wn.getNodeId());
-							coordinates[i] = n[0];
-							coordinates[i + 1] = n[1];
-							i = i + 2;
-						}
-						ways.add(coordinates);
-					}
-					break;
-				default:
-					break;
-				}
-			}
-		});
-		reader.run();
-		RTree<double[], Geometry> tree = RTree.create();
-		for (double[] coordinates : ways) {
-			double minLat = coordinates[1];
-			double maxLat = coordinates[1];
-			double minLon = coordinates[0];
-			double maxLon = coordinates[0];
-			for (int i = 0; i < coordinates.length; i++) {
-				if (i % 2 == 0) {
-					minLon = Math.min(minLon, coordinates[i]);
-					maxLon = Math.max(maxLon, coordinates[i]);
-				} else {
-					minLat = Math.min(minLat, coordinates[i]);
-					maxLat = Math.max(maxLat, coordinates[i]);
-				}
-			}
-			tree = tree.add(coordinates, Geometries.rectangleGeographic(minLon, minLat, maxLon, maxLat));
-		}
-		tree.visualize(600, 600).save("target/mytree.png");
-		System.out.println("Loading OK");
-		return tree;
-	}
+    private static CoordinateSequence sequence(BoundingBox bbox, StoredPolygon stored) {
+        float[] way = stored.getCoordinates();
+        Coordinate[] array = new Coordinate[way.length / 2];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Coordinate(bbox.relativeLon(way[2 * i]), bbox.relativeLat(way[2 * i + 1]));
+        }
+        return geometryFactory.getCoordinateSequenceFactory().create(array);
+    }
 }
